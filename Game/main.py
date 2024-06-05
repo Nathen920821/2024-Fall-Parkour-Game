01386ddde1a3466bf14e0996abfe176e43a481a1
Nathen920821 <112932434+Nathen920821@users.noreply.github.com>
2024-06-06 02:55:17 +0800

    Add files via upload

--- Game/main.py
+++ Game/main.py
@@ -16,12 +16,12 @@ def receive_data():
             break
 
 def update():
-    global offset, collision, movement, score, life, data, t1, movement, movement_counter
+    global offset, collision, movement, score, life, data, t1, movement, movement_counter, fly, fly_start
 
     #Variable setting
     acceleration = score / 10000000
     score += 1 / 10
-    player_speed = time.dt * 0.15
+    player_speed = time.dt * 0.1
     offset += player_speed + acceleration
     train_speed = [time.dt * 0.45, time.dt * 0.25]
     train_spawn = [2, 1.2]
@@ -37,7 +37,7 @@ def update():
                     choose += 1
                 data = buffer_data[choose] + '}'
             obj = json.loads(data)
-            print(data)
+            #print(data)
             if movement != "":
                 if movement_counter < 20:
                     match movement:
@@ -82,8 +82,12 @@ def update():
                     t1 = time.time()
         except json.JSONDecodeError:
             pass
-    camera.position = (player.x, 6, -125)
     
+    if player.y > 8.1:
+        camera.position = (player.x, player.y + 5, -125)
+    else:
+        camera.position = (player.x, 6, -125)
+
     setattr(rail0, "texture_offset", (0, offset))
     setattr(rail1, "texture_offset", (0, offset))
     setattr(rail2, "texture_offset", (0, offset))
@@ -91,8 +95,18 @@ def update():
     #Collision
     hit_info = player.intersects()
     if hit_info.hit:
-        collision = True
-        player.y = 8
+        if hit_info.entity not in items:
+            collision = True
+            if player.y < 8:
+                player.y = 8
+            if hit_info.entity in dragons:
+                fly = False
+        elif hit_info.entity == flying_item:
+            fly = True
+            dragon1.z = 0.2
+            dragon2.z = 1
+            fly_start = time.time()
+            flying_item.position = (1 * random.randint(-1, 1), 5, random.uniform(4,6))
     if collision:
         player.rotation_x += time.dt * 400
         player.color = color.red
@@ -101,6 +115,19 @@ def update():
             player.color = color.white
             player.rotation_x = 0
             life -= 1
+
+    #flying
+    if fly:
+        if player.y < 50:
+            player.y += 1
+        else:
+            player.y = 50
+        fly_end = time.time()
+        if fly_end - fly_start < 6:
+            gravity = 0
+        else:
+            gravity = 0.05
+            fly = False
 
     #Life and Score
     Score.text = f'Score : {int(score)}'
@@ -111,15 +138,30 @@ def update():
 
     #Gravity
     if player.y > 1:
-        player.y -= gravity
+        if player.y > 8:
+            player.y -= 1.4*gravity
+        else:
+            player.y -= gravity
     if player.y < 1:
         player.y = 1
+
+    #Item
+    for item in items:
+        item.z -= player_speed + acceleration
+        if item.z < -0.7:
+            item.position = (1 * random.randint(-1, 1), 5, random.uniform(4,6))
 
     #Block
     for block in blocks:
         block.z -= player_speed + acceleration
         if block.z < -0.7:
             block.position = (1 * random.randint(-1, 1), 0.1, random.uniform(0.2, 1))
+
+    #Dragon
+    for dragon in dragons:
+        dragon.z -= 2.2*player_speed + acceleration
+        if dragon.z < -0.7:
+            dragon.position = (random.choice([-1, 1]), 86, random.uniform(0.2, 1))
 
     #Train
     for train in trains:
@@ -161,7 +203,7 @@ class Rail(Entity):
         super().__init__()
         self.model = "cube"
         self.texture = "assets/rail.png"
-        self.scale = (8, 0.5, 600)
+        self.scale = (8, 0.5, 1200)
         self.position = position
 
 class Train(Entity):
@@ -174,6 +216,18 @@ class Train(Entity):
         self.position = position
         self.collider = "box"
         self.rotation_y = angle
+
+class Dragon(Entity):
+    def __init__(self, position, angle):
+        super().__init__()
+        self.model = "assets/dragon.obj"
+        self.texture = "assets/dragon.png"
+        self.scale = (0.00005, 0.04, 0.0015)
+        self.position = position
+        self.collider = "box"
+        self.parent = rail0
+        self.rotation_y = angle
+        self.rotation_y = 90
 
 class Block(Entity):
     def __init__(self):
@@ -217,6 +271,7 @@ running = True
 conn = socket.socket()
 offset = 0
 collision = False
+fly = False
 movement = ""
 movement_counter = 0
 score = 0
@@ -229,11 +284,18 @@ rail0 = Rail((0, 0, 0))
 rail1 = Rail((-8, 0, 0))
 rail2 = Rail((8, 0, 0))
 
-sky = Entity(model = "cube", texture = "assets/sky.jpg", scale = (100, 1200, 0.0001), parent = rail0, z = 1.5, y = 0.2)
+sky = Entity(model = "cube", texture = "assets/sky.jpg", scale = (500, 6000, 0.0005), parent = rail0, z = 1.5, y = 0.2)
+
+flying_item = Entity(model = "cube", texture = "assets/flying_item.jpg", scale = (0.3, 4, 0.01), parent = rail0, position = (0,5,3), collider = "box")
+items = [flying_item]
 
 train1 = Train((1, 1.2, .2), 0)
 train2 = Train((-1, 1.2, .2), 180)
 trains = [train1, train2]
+
+dragon1 = Dragon((1, 86, .2), 0)
+dragon2 = Dragon((1, 86, .4), 0)
+dragons = [dragon1, dragon2]
 
 blocks = []
 for i in range(7):
@@ -255,6 +317,9 @@ print("Connected at", addr)
 threading.Thread(target=receive_data, daemon=True).start()
 
 t1 = time.time()
+fly_start = 0
+
+print("game start")
 
 app.run()
 running = False
